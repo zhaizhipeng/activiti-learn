ec3aeeb2f9445b437a73a7c3dbe251c032733802
zhaizhipeng <zhaizhipeng@extracme.com>
2020-06-12 09:00:08 +0800

    流程变量

--- src/main/java/com/ysdrzp/e_processVariables/TestProcessVariables.java
+++ src/main/java/com/ysdrzp/e_processVariables/TestProcessVariables.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 
 /**
  * 流程变量测试
- * 相关表 - act_ru_variable、act_hi_varinst
+ * 涉及的表 - act_ru_variable、act_hi_varinst
  * @author ysdrzp
  *
  */
@@ -30,9 +30,9 @@ public  class TestProcessVariables {
 	 */
 	@Test
 	public void deployProcess() {
-		// 得到流程部署的service
 		RepositoryService repositoryService = this.processEngine.getRepositoryService();
-		Deployment deploy = repositoryService.createDeployment().name("请假流程001").addClasspathResource("HelloWorld.bpmn")
+		Deployment deploy = repositoryService.createDeployment().name("请假流程001")
+				.addClasspathResource("HelloWorld.bpmn")
 				.addClasspathResource("HelloWorld.png").deploy();
 		System.out.println("部署成功:流程部署ID：" + deploy.getId());
 	}
@@ -42,18 +42,27 @@ public  class TestProcessVariables {
 	 */
 	@Test
 	public void startProcess() {
+
 		RuntimeService runtimeService = this.processEngine.getRuntimeService();
+
+		/**
+		 * 流程定义Key
+		 */
 		String processDefinitionKey = "HelloWorld";
-//		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processDefinitionKey);
-		//创建流程变量对象
+
+		/**
+		 * 流程变量对象
+		 */
 		Map<String,Object> variables=new HashMap<>();
-		variables.put("请假天数", 5);//int
+		variables.put("请假天数", 5);
 		variables.put("请假原因", "约会");
 		variables.put("请假时间", new Date());
-		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processDefinitionKey, variables);
-		System.out.println("流程启动成功:" + processInstance.getId() + "   " + processInstance.getProcessDefinitionId() + "  "
-				+ processInstance.getProcessInstanceId());
 
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processDefinitionKey, variables);
+		System.out.println("=================================流程启动成功=================================");
+		System.out.println("流程执行ID:" + processInstance.getId());
+		System.out.println("流程定义ID:" + processInstance.getProcessDefinitionId());
+		System.out.println("流程实例ID:" + processInstance.getProcessInstanceId());
 	}
 	
 	/**
@@ -61,14 +70,16 @@ public  class TestProcessVariables {
 	 */
 	@Test
 	public void setVariables() {
+
 		RuntimeService runtimeService = this.processEngine.getRuntimeService();
-		String executionId="2501";
-		//runtimeService.setVariable(executionId, "请假人", "小明");
+		String executionId="7501";
+
 		Map<String,Object> variables=new HashMap<>();
-		variables.put("请假天数", 6);//int
+		variables.put("请假天数", 6);
 		variables.put("请假原因", "约会妹子");
 		variables.put("请假时间", new Date());
 		variables.put("用户对象", new User(1,"小明"));
+
 		runtimeService.setVariables(executionId, variables);
 		System.out.println("流程变量设置成功");
 	}
@@ -78,80 +89,66 @@ public  class TestProcessVariables {
 	 */
 	@Test
 	public void setVariables2() {
+
+		/**
+		 * 流程执行ID
+		 */
 		TaskService taskService = this.processEngine.getTaskService();
 		
-		String taskId="2507";
-		//runtimeService.setVariable(executionId, "请假人", "小明");
+		String taskId="7507";
 		Map<String,Object> variables=new HashMap<>();
-		variables.put("任务ID设置的", 9);//int
-//		taskService.setVariable(taskId, variableName, value);
+		variables.put("任务ID设置", 9);
+
 		taskService.setVariables(taskId, variables);
 		System.out.println("流程变量设置成功");
 	}
-	
 	
 	/**
 	 * 获取流程变量
 	 */
 	@Test
 	public void getVariables() {
+
 		RuntimeService runtimeService = this.processEngine.getRuntimeService();
-		String executionId="2501";
+
+		/**
+		 * 流程执行ID
+		 */
+		String executionId = "7501";
+
 		Integer days=(Integer) runtimeService.getVariable(executionId, "请假天数");
 		Date date=(Date) runtimeService.getVariable(executionId, "请假时间");
 		User user=(User) runtimeService.getVariable(executionId, "用户对象");
-		System.out.println(days);
-		System.out.println(date.toLocaleString());
-		System.out.println(user.getId()+"  "+user.getName());
+
+		System.out.println("请假天数：" + days);
+		System.out.println("请假时间：" + date.toLocaleString());
+		System.out.println("用户对象：" + user);
 	}
 	
 	/**
-	 * 7：查询历史的流程变量
+	 * 查询历史的流程变量
 	 */
 	@Test
 	public void getHistoryVariables() {
+
 		HistoryService historyService = this.processEngine.getHistoryService();
+
+		/**
+		 * 流程实例ID
+		 */
+		String processInstanceId = "7501";
+		List<HistoricVariableInstance> list = historyService.createHistoricVariableInstanceQuery()
+				.processInstanceId(processInstanceId)
+				.list();
 		
-		/*HistoricVariableInstance singleResult = historyService.createHistoricVariableInstanceQuery().id("2503").singleResult();;
-		System.out.println(singleResult.getId());
-		System.out.println(singleResult.getValue());
-		System.out.println(singleResult.getVariableName());
-		System.out.println(singleResult.getVariableTypeName());*/
-		String processInstanceId="2501";
-		List<HistoricVariableInstance> list = historyService.createHistoricVariableInstanceQuery().processInstanceId(processInstanceId).list();
-		
-		for (HistoricVariableInstance hvs : list) {
-			System.out.println("ID"+hvs.getId());
-			System.out.println("变量值"+hvs.getValue());
-			System.out.println("变量名"+hvs.getVariableName());
-			System.out.println("变量类型"+hvs.getVariableTypeName());
+		for (HistoricVariableInstance historicVariableInstance : list) {
+			System.out.println("ID：" + historicVariableInstance.getId());
+			System.out.println("变量值：" + historicVariableInstance.getValue());
+			System.out.println("变量名:" + historicVariableInstance.getVariableName());
+			System.out.println("变量类型：" + historicVariableInstance.getVariableTypeName());
+			System.out.println("流程实例ID：" + historicVariableInstance.getProcessInstanceId());
 			System.out.println("#####################");
 		}
-	}
-
-	/**
-	 * 设置流程变量2
-	 */
-	@Test
-	public void setVariables3() {
-		RuntimeService runtimeService = this.processEngine.getRuntimeService();
-		String executionId="2501";
-		//runtimeService.setVariable(executionId, "请假人", "小明");
-		Map<String,Object> variables=new HashMap<>();
-		variables.put("测试", "约会妹子");
-		runtimeService.setVariablesLocal(executionId, variables);
-		System.out.println("流程变量设置成功");
-	}
-	
-	@Test
-	public void setVariables4() {
-		TaskService taskService = this.processEngine.getTaskService();
-		String taskId="2507";
-		//runtimeService.setVariable(executionId, "请假人", "小明");
-		Map<String,Object> variables=new HashMap<>();
-		variables.put("测试2", 9);//int
-		taskService.setVariablesLocal(taskId, variables);
-		System.out.println("流程变量设置成功");
 	}
 
 }
--- src/main/java/com/ysdrzp/e_processVariables/User.java
+++ src/main/java/com/ysdrzp/e_processVariables/User.java
@@ -4,15 +4,11 @@ import java.io.Serializable;
 
 public class User implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+
 	private Integer id;
+
 	private String name;
-	public User() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	public User(Integer id, String name) {
 		super();
@@ -20,18 +16,27 @@ public class User implements Serializable{
 		this.name = name;
 	}
 
-
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
 }
